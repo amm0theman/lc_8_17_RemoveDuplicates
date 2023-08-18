6abe4b3340d5991a8ba86a1cc27d6cc0be6a3270
amm0theman <ammonriley.97@gmail.com>
2023-08-17 20:04:04 -0600

    Add project files.

---
+++ lc_8_17_RemoveDuplicates/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        Dictionary<int, int> result = new Dictionary<int, int>();
+        int position = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (result.TryAdd(nums[i], nums[i]))
+            {
+                nums[position] = nums[i];
+                position++;
+            }
+        }
+
+        return result.Count;
+    }
+}
